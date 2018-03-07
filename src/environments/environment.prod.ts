cce5c7cc4c4898aefb85d1a1caf4525fb1f90985
famaridon <famaridon@visiativ.com>
2018-03-07 23:14:54 +0100

    add storage to production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,7 +5,7 @@ export const environment = {
     authDomain: 'lunch-change-45b17.firebaseapp.com',
     databaseURL: 'https://lunch-change-45b17.firebaseio.com',
     projectId: 'lunch-change-45b17',
-    storageBucket: '',
+    storageBucket: 'lunch-change-45b17.appspot.com',
     messagingSenderId: '742433379934'
   }
 };
